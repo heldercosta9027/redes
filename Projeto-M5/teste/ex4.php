ae23b7a15f93585b3765cf18ef457bb4610c8beb
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-12-10 11:50:45 +0000

    Update ex4.php

--- Projeto-M5/teste/ex4.php
+++ Projeto-M5/teste/ex4.php
@@ -1,25 +1,39 @@
 <?php
-        $numero = array();
-            for($i=0; $i<20; $i++){
-                $numero[$i]=rand(1,10000);
-            }
-         
-        
-        $produto=0;
-        $media=0;
-        
-        echo "<h3>Ex4</h3>";
-
-        foreach($numero as $n){
-            echo $n. " ";
-        }
-        echo "<br><br>";
-        
-                     
-        for ($i=0 ;$i<20 ;$i++){
-        $produto = $numero[$i]* $produto;
-        echo "O produto é: ". $produto;
+    $produto=1;
+    $soma=0;
+    for($i=0;$i<20;$i++){
+        $numero=rand(1,10000);
+        $vetor[$i]=$numero;
     }
-               
-           
-?>
+    echo "<h3>Ex4</h3>";
+
+
+    $maior=$vetor[0];
+    $menor=$vetor[0];
+    
+
+    for($i=0;$i<20;$i++){
+        $produto = $produto * $vetor[$i];
+        $soma=$soma+$vetor[$i];
+    }
+
+    $media=$soma/20;
+    
+    foreach ($vetor as $n){
+        if($n>$maior){
+            $maior=$n;
+        }     
+    }
+
+    foreach($vetor as $n){
+        if($n<$menor){
+            $menor=$n;
+        }     
+    }
+    
+    
+    echo "<h4>O maior número é: ".$maior. "<br><br></h4>"; 
+    echo "<h4>O menor número é: ".$menor. "<br><br></h4>"; 
+    echo "<h4>O produto deles é: ".$produto. "<br><br></h4>";
+    echo "<h4>A média deles é: ".$media. "<br></h4>";
+?> 
